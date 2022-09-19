508ddd834c6d6ca16e1dc1dd30c8f15e4b432a9e
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-19 12:05:37 +0530

    Added: Welcome to Matrix Program

---
+++ src/com/blz/java/Functions2Darray.java
@@ -0,0 +1,12 @@
+package com.blz.java;
+
+public class Functions2Darray {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Computation 2D Array Computation Program!!");
+		
+		
+
+	}
+
+}
